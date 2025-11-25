752b90f08196a9e9ce6b46e9299a641fcf20861b
drodrigor <danierrodrigo@gmail.com>
2025-11-25 13:56:42 +0100

    Proyecto del día. Pregunta 2

---
+++ CODIGO/Dia 5/4 - Proyecto del día/misScripts.js
@@ -0,0 +1,51 @@
+function recomendar(genero) {
+    let elementoEdad = document.getElementById("edad").value;
+    let elementoRespuesta = document.getElementById("peliculaRecomendada");
+
+    switch (genero && edad < 13) {
+        case 'accion':
+            
+            break;
+        case 'drama':
+            
+            break;
+        case 'clasicos':
+            
+            break;
+        case 'disney':
+            
+            break;
+    }
+
+    switch (genero && (edad >= 13 && edad < 16)) {
+        case 'accion':
+            
+            break;
+        case 'drama':
+            
+            break;
+        case 'clasicos':
+            
+            break;
+        case 'disney':
+            
+            break;
+    }
+
+    switch (genero && edad >= 16) {
+        case 'accion':
+            
+            break;
+        case 'drama':
+            
+            break;
+        case 'clasicos':
+            
+            break;
+        case 'disney':
+            
+            break;
+    }
+
+    
+}
