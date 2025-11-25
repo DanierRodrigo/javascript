b404b91552e6a76effb98dd142f7e90f3aa66b10
drodrigor <danierrodrigo@gmail.com>
2025-11-25 16:35:18 +0100

    Proyecto del día. Pregunta 3

--- CODIGO/Dia 5/4 - Proyecto del día/misScripts.js
+++ CODIGO/Dia 5/4 - Proyecto del día/misScripts.js
@@ -4,46 +4,46 @@ function recomendar(genero) {
 
     switch (genero && edad < 13) {
         case 'accion':
-            
+            elementoRespuesta.textContent = "Hook: El Capitán Garfio";
             break;
         case 'drama':
-            
+            elementoRespuesta.textContent = "Solo en Casa";
             break;
         case 'clasicos':
-            
+            elementoRespuesta.textContent = "Mary Poppings";
             break;
         case 'disney':
-            
+            elementoRespuesta.textContent = "Toy Story";
             break;
     }
 
     switch (genero && (edad >= 13 && edad < 16)) {
         case 'accion':
-            
+            elementoRespuesta.textContent = "To Fast To Furious";
             break;
         case 'drama':
-            
+            elementoRespuesta.textContent = "Mi chica";
             break;
         case 'clasicos':
-            
+            elementoRespuesta.textContent = "El bueno, el feo y el malo";
             break;
         case 'disney':
-            
+            elementoRespuesta.textContent = "El rey león";
             break;
     }
 
     switch (genero && edad >= 16) {
         case 'accion':
-            
+            elementoRespuesta.textContent = "Django: Desencadenado";
             break;
         case 'drama':
-            
+            elementoRespuesta.textContent = "Atrápame si puedes";
             break;
         case 'clasicos':
-            
+            elementoRespuesta.textContent = "Scarface";
             break;
         case 'disney':
-            
+            elementoRespuesta.textContent = "Star Wars: La amenaza fantasma";
             break;
     }
 
