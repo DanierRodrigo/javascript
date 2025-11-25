f0ab46c430210070b3f225b96697321d095b6ff5
drodrigor <danierrodrigo@gmail.com>
2025-11-25 17:03:12 +0100

    Proyecto del día. Pregunta 3

--- CODIGO/Dia 5/4 - Proyecto del día/misScripts.js
+++ CODIGO/Dia 5/4 - Proyecto del día/misScripts.js
@@ -1,51 +1,47 @@
 function recomendar(genero) {
-    let elementoEdad = document.getElementById("edad").value;
+    let elementoEdad = parseInt(document.getElementById("textoEdad").value);
     let elementoRespuesta = document.getElementById("peliculaRecomendada");
 
-    switch (genero && edad < 13) {
+    switch (genero) {
+
         case 'accion':
-            elementoRespuesta.textContent = "Hook: El Capitán Garfio";
+            if (elementoEdad < 13) {
+                elementoRespuesta.textContent = "Hook: El Capitán Garfio";
+            } else if (elementoEdad < 16) {
+                elementoRespuesta.textContent = "Too Fast Too Furious";
+            } else {
+                elementoRespuesta.textContent = "Django: Desencadenado";
+            }
             break;
+
         case 'drama':
-            elementoRespuesta.textContent = "Solo en Casa";
+            if (elementoEdad < 13) {
+                elementoRespuesta.textContent = "Solo en Casa";
+            } else if (elementoEdad < 16) {
+                elementoRespuesta.textContent = "Mi chica";
+            } else {
+                elementoRespuesta.textContent = "Atrápame si puedes";
+            }
             break;
+
         case 'clasicos':
-            elementoRespuesta.textContent = "Mary Poppings";
+            if (elementoEdad < 13) {
+                elementoRespuesta.textContent = "Mary Poppins";
+            } else if (elementoEdad < 16) {
+                elementoRespuesta.textContent = "El bueno, el feo y el malo";
+            } else {
+                elementoRespuesta.textContent = "Scarface";
+            }
             break;
+
         case 'disney':
-            elementoRespuesta.textContent = "Toy Story";
+            if (elementoEdad < 13) {
+                elementoRespuesta.textContent = "Toy Story";
+            } else if (elementoEdad < 16) {
+                elementoRespuesta.textContent = "El rey león";
+            } else {
+                elementoRespuesta.textContent = "Star Wars: La amenaza fantasma";
+            }
             break;
     }
-
-    switch (genero && (edad >= 13 && edad < 16)) {
-        case 'accion':
-            elementoRespuesta.textContent = "To Fast To Furious";
-            break;
-        case 'drama':
-            elementoRespuesta.textContent = "Mi chica";
-            break;
-        case 'clasicos':
-            elementoRespuesta.textContent = "El bueno, el feo y el malo";
-            break;
-        case 'disney':
-            elementoRespuesta.textContent = "El rey león";
-            break;
-    }
-
-    switch (genero && edad >= 16) {
-        case 'accion':
-            elementoRespuesta.textContent = "Django: Desencadenado";
-            break;
-        case 'drama':
-            elementoRespuesta.textContent = "Atrápame si puedes";
-            break;
-        case 'clasicos':
-            elementoRespuesta.textContent = "Scarface";
-            break;
-        case 'disney':
-            elementoRespuesta.textContent = "Star Wars: La amenaza fantasma";
-            break;
-    }
-
-    
 }
