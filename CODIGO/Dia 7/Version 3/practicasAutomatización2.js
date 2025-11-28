464c945e0fa382cecce5678da17a3e1c3de3bb8b
drodrigor <danierrodrigo@gmail.com>
2025-11-28 14:48:44 +0100

    Ejercicio 73 Resuelto

--- CODIGO/Dia 7/Version 3/practicasAutomatización2.js
+++ CODIGO/Dia 7/Version 3/practicasAutomatización2.js
@@ -9,6 +9,26 @@ que lo vincule con txtInput y al input agregarle
 el atributo type igual a number y un atributo valor igual a 0.*/
 
 function generarContenido() {
-    
+    //Obtener el div contenedor
+    let contenedor = document.getElementById("contenido");
+
+    //Creamos el label
+    let label = document.createElement("label");
+    label.textContent = "Label 1";
+
+    //Creamos el input
+    let input = document.createElement("input");
+
+    //Agregamos los atributos
+    input.setAttribute("id","txtInput");
+    input.setAttribute("type","number");
+    input.setAttribute("value", 0);
+
+    //Conectar el label con el input mediante su ID
+    label.setAttribute("for", "txtInput");
+
+    //Agregar el label al div
+    contenedor.appendChild(label);
+    contenedor.appendChild(input);
 }
 
