6c67f218b2cc52ca20c27aaed938a584c5b180c5
DanierRodrigo <danierrodrigo@gmail.com>
2025-11-30 23:58:57 +0100

    Apuntes Versión 4 Proyecto 7

---
+++ CODIGO/Dia 7/Version 4/misScripts.js
@@ -0,0 +1,106 @@
+function crearTiendas(contenedorID, min, cantidadTiendas){
+    //Encontrar contenedor por su id
+    let elementoContenedor = document.getElementById(contenedorID);
+
+    //Loop para crear tantas tiendas como se desee
+    for(let contadorTiendas = 1; contadorTiendas <= cantidadTiendas; contadorTiendas++){
+
+        //Crear el texto de label para poder llamar a la función
+        let textoEtiqueta = "Tienda " + contadorTiendas;
+
+        //Crear tiendas con crearParrafoTienda
+        let parrafoTienda = crearParrafoTienda(textoEtiqueta, min);
+    }
+}
+
+function crearParrafoTienda(textoLabel, valorMin){
+    //Crear las atiquetas <p>
+    let elementoParrafo = document.createElement("p");
+
+    //Crear la etiqueta label
+    let elementoEtiqueta = document.createElement("label");
+
+    //Conectar con el input
+    elementoEtiqueta.setAttribute("for", textoLabel);
+
+    //Crear el input
+    let elementoInput = document.createElement("input");
+
+    //Establecer atributos de input
+    elementoInput.setAttribute("type","number");
+    elementoInput.setAttribute("id", textoLabel);
+    elementoInput.setAttribute("min", valorMin);
+    elementoInput.setAttribute("value", 0);
+
+    //Añadir label e input al párrafo
+    elementoParrafo.appendChild(elementoEtiqueta);
+    elementoParrafo.appendChild(elementoInput);
+
+    //Devolver el párrafo completo
+    return elementoParrafo;
+}
+
+function extraerNumeroDesdeElemento(elemento){
+    let miElemento = document.getElementById(elemento);
+    let miTexto = miElemento.value;
+    let miNumero = Number(miTexto);
+
+    return miNumero;
+}
+
+function calcular(){
+    let ventas = [];
+
+    ventas[0] = extraerNumeroDesdeElemento("ventasTienda1");
+    ventas[1] = extraerNumeroDesdeElemento("ventasTienda2");
+    ventas[2] = extraerNumeroDesdeElemento("ventasTienda3");
+    ventas[3]= extraerNumeroDesdeElemento("ventasTienda4");
+    ventas[4]= extraerNumeroDesdeElemento("ventasTienda5");
+    ventas[5] = extraerNumeroDesdeElemento("ventasTienda6");
+
+    let totalVentas = sumarTotal(ventas);
+    let ventaMayor = calcularMayor(ventas);
+    let ventaMenor = calcularMenor(ventas);
+
+    let mensajeSalida = "Total Ventas: " + totalVentas +
+                        " / Mayor venta: " + ventaMayor +
+                        " / Menor venta: " + ventaMenor;
+    let elementoSalida = document.getElementById("parrafoSalida");
+
+    elementoSalida.textContent = mensajeSalida;
+
+}
+
+function sumarTotal(array){
+    let total = 0;
+
+    for(let venta of array){
+        total = total + venta;
+    }
+
+    return total;
+}
+
+function calcularMayor(array){
+    let maximo = array[0];
+
+    for(let venta of array){
+        if(venta > maximo){
+            maximo = venta;
+        }
+    }
+
+    return maximo;
+}
+
+function calcularMenor(array){
+    let minimo = array[0];
+
+    for(let venta of array){
+        if(venta < minimo){
+            minimo = venta;
+        }
+    }
+
+    return minimo;
+}
