f95f7224b3c2a1478a48031190872f0e09103a70
drodrigor <danierrodrigo@gmail.com>
2025-12-01 17:29:02 +0100

    Ejercicio 74 Enunciado

---
+++ CODIGO/Dia 7/Version 4/practicasAutomatizacion3.js
@@ -0,0 +1,33 @@
+/*Adapta la función generarContenido, 
+para que utilizando el parámetro numero, 
+el input que se genera (y su correspondiente label asociado) 
+utilice este parámetro para definir su ID. 
+
+Es decir, que si el parámetro numero es 3, 
+el ID del input generado deberá ser txtInput3 
+y además el label se debe asociar a ese ID del input 
+y el texto que muestra debe decir Label 3.
+
+El objetivo de esto, sería que luego se pueda generar código 
+de forma repetitiva como se vio en la última clase 
+y que los nombres de los campos se generen de forma dinámica*/
+
+function generarContenido(numero) {
+    let content = document.getElementById("contenido");
+    
+    let label = document.createElement("label");
+    label.setAttribute("for", "txtInput");
+    label.innerText = "Label";
+    
+    
+    let input = document.createElement("input");
+    input.setAttribute("id", "txtInput");
+    input.setAttribute("type", "number");
+    input.setAttribute("value", 0);
+    
+    content.appendChild(label);
+    content.appendChild(input);
+    
+    return content;
+}
+
