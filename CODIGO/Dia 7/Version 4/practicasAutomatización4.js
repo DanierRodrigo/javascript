1fe7f9115600386828cbe170768740dbbbb919b1
drodrigor <danierrodrigo@gmail.com>
2025-12-02 13:36:28 +0100

    Ejercicio 75 Enunciado

---
+++ CODIGO/Dia 7/Version 4/practicasAutomatización4.js
@@ -0,0 +1,32 @@
+/**
+Implementar la función repetirContenido, 
+para que dado el parámetro veces genere esa misma cantidad 
+de elemento llamando a la función generarContenido ya declarada. 
+
+Es decir que si el parámetro veces es igual a 5, 
+se deberán generar (mediante la función generarContenido) 
+5 elementos input con sus respectivos label.
+ */
+
+function repetirContenido(veces) {
+    
+}
+
+function generarContenido(numero) {
+    let content = document.getElementById("contenido");
+    
+    let label = document.createElement("label");
+    label.setAttribute("for", "txtInput" + numero);
+    label.innerText = "Label " + numero;
+    
+    
+    let input = document.createElement("input");
+    input.setAttribute("id", "txtInput" + numero);
+    input.setAttribute("type", "number");
+    input.setAttribute("value", 0);
+    
+    content.appendChild(label);
+    content.appendChild(input);
+    
+    return content;
+}
