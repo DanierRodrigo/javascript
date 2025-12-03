611f772c225b39cd58f80e5e41f194f9454342cb
drodrigor <danierrodrigo@gmail.com>
2025-12-03 16:13:50 +0100

    Ejercicio 76 Enunciado

---
+++ CODIGO/Dia 7/Version 5/practicasAutomatizacion5.js
@@ -0,0 +1,20 @@
+/**Haciendo uso de lo visto en la clase anterior, 
+implementar la función automatizarSuma() 
+que debe calcular y guardar el resultado de sumar 
+el contenido de los inputs en la variable sumaTotal. 
+Para ello, hacer uso de la propiedad children vista 
+y tener en cuenta que la estructura de elementos 
+en nuestra página HTML es la siguiente:
+
+<div id="contenido">
+
+<p>
+    <label>
+    <input> 
+*/
+
+let sumaTotal = 0;
+
+function automatizarSuma() {
+
+}
