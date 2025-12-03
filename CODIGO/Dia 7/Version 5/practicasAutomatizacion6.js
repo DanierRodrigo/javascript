2cb925e04da7fd9d8fc0c6d2f18be042516eb10c
drodrigor <danierrodrigo@gmail.com>
2025-12-03 16:24:48 +0100

    Ejercicio 77 Resuelto

--- CODIGO/Dia 7/Version 5/practicasAutomatizacion6.js
+++ CODIGO/Dia 7/Version 5/practicasAutomatizacion6.js
@@ -17,5 +17,17 @@ de nuestra página HTML y cambio el índice para acceder a los elementos input. 
 let sumaTotal = 0;
 
 function automatizarSuma() {
+    sumaTotal = 0; // Reiniciar antes de sumar
 
+    let contenedor = document.getElementById("contenido");
+
+    // Recorrer todos los <p> que son hijos directos de "contenido"
+    for (let i = 0; i < contenedor.children.length; i++) {
+
+        let parrafo = contenedor.children[i];       // <p>
+        let input = parrafo.children[0];            // el <input> dentro del <p>
+
+        let valor = Number(input.value);            // obtener valor numérico
+        sumaTotal += valor;                         // acumular
+    }
 }
