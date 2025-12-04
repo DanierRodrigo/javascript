43812747658907731ee7b47c0ad83ed0cfcad4f9
drodrigor <danierrodrigo@gmail.com>
2025-12-04 18:24:52 +0100

    Ejercicio 78 Enunciado

---
+++ CODIGO/Día 8/1 - Objetos/practicasObjetos1.js
@@ -0,0 +1,11 @@
+/**
+Crea un objeto llamado automovil, con las siguientes propiedades y métodos:
+
+marca: Ford
+
+modelo: Fiesta
+
+anio: 2015
+
+arrancar(): Debe mostrar en consola el mensaje "En marcha"
+ */
