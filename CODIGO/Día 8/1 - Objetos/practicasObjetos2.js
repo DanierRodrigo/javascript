db94f69bd649af76e1df5908a17815ac3e4cd8fa
drodrigor <danierrodrigo@gmail.com>
2025-12-04 18:37:29 +0100

    Ejercicio 79 Enunciado

---
+++ CODIGO/Día 8/1 - Objetos/practicasObjetos2.js
@@ -0,0 +1,11 @@
+/**
+Crea un objeto llamado polo, con las siguientes propiedades y métodos:
+
+marca: Lacoste
+
+color: Azul
+
+talle: L
+
+fabricar(): Debe mostrar en consola el mensaje "Polo fabricado"
+ */
