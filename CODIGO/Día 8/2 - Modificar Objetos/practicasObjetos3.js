4ab5d643e42ed2548c14decc31331ea1b9c5fedb
DanierRodrigo <danierrodrigo@gmail.com>
2025-12-05 22:06:19 +0100

    Ejercicio Objetos 3 Enunciado

---
+++ CODIGO/Día 8/2 - Modificar Objetos/practicasObjetos3.js
@@ -0,0 +1,16 @@
+/**
+Implementar la función agregarPropiedad, 
+para que le agregue al objeto polo, 
+la propiedad o atributo genero y lo inicialice como 'Masculino'
+ */
+
+let polo = { 
+    marca: 'Lacoste', 
+    color: 'Azul', 
+    talle: 'L', 
+    fabricar() { console.log("Polo fabricado") }
+};
+
+function agregarPropiedad() {
+    
+}
