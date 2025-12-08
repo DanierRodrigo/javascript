e12674e393cafced24974421c93b9b2c07123b4a
DanierRodrigo <danierrodrigo@gmail.com>
2025-12-08 19:50:56 +0100

    Ejercicio Constructores 2 Enunciado

---
+++ CODIGO/Día 8/4 - Constructores/practicasConstructores2.js
@@ -0,0 +1,12 @@
+/**
+Crea un constructor para el objeto Polo e inicializa las propiedades/atributos con los siguientes valores:
+
+marca: Lacoste
+
+color: Azul
+
+talle: L
+
+fabricar(): Debe mostrar en consola el mensaje "Polo fabricado"
+ */
+
