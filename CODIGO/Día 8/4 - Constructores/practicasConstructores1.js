a13c3d6891f937391ad196421e1729cd2530151d
DanierRodrigo <danierrodrigo@gmail.com>
2025-12-08 19:21:44 +0100

    Ejercicio Constructores  1 Enunciado

---
+++ CODIGO/Día 8/4 - Constructores/practicasConstructores1.js
@@ -0,0 +1,12 @@
+/**
+Crea un constructor para el objeto Automovil 
+e inicializa las propiedades/atributos con los siguientes valores:
+
+marca: Ford
+
+modelo: Fiesta
+
+anio: 2015
+
+arrancar(): Debe mostrar en consola el mensaje "En marcha"
+ */
