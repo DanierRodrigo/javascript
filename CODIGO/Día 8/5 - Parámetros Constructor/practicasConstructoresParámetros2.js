f9bed80595801066fa892df2e0fc78f371189ca7
drodrigor <danierrodrigo@gmail.com>
2025-12-09 18:27:17 +0100

    Ejercicio Parámetros Constructores 2 Resuelto

--- CODIGO/Día 8/5 - Parámetros Constructor/practicasConstructoresParámetros2.js
+++ CODIGO/Día 8/5 - Parámetros Constructor/practicasConstructoresParámetros2.js
@@ -2,3 +2,9 @@
 Crea un constructor para el objeto Polo 
 con parámetros para definir los atributos: marca, color y talle.
  */
+
+function Polo(marca, color, talle){
+    this.marca = marca;
+    this.color = color;
+    this.talle = talle;
+}
