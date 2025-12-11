36506b0ac8c08759c9f114dac4a616b2b7f007a9
drodrigor <danierrodrigo@gmail.com>
2025-12-11 12:38:12 +0100

    Proyecto 8 Pregunta 3

--- CODIGO/Día 8/Proyecto del día 8/misScripts.js
+++ CODIGO/Día 8/Proyecto del día 8/misScripts.js
@@ -3,11 +3,27 @@ const empleados = [];
 
 // Constructor de Empleado
 class Empleado {
-constructor(nombre, apellido, edad, departamento, salario) {
-this.nombre = nombre;
-this.apellido = apellido;
-this.edad = edad;
-this.departamento = departamento;
-this.salario = salario;
+    constructor(nombre, apellido, edad, departamento, salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.departamento = departamento;
+        this.salario = salario;
+    }
 }
-}
+
+//Agregar Empleados
+document.getElementById('agregar').addEventListener('click', () => {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const edad = document.getElementById('edad').value;
+    const departamento = document.getElementById('departamento').value;
+    const salario = document.getElementById('salario').value;
+
+
+    const nuevoEmpleado = new Empleado(nombre, apellido, edad, departamento, salario);
+    empleados.push(nuevoEmpleado);
+
+
+    alert('Empleado agregado correctamente');
+});
