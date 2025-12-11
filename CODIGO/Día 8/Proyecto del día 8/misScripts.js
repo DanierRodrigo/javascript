deeeaafbe682e99dc7b57b05e22a4fdb5d9d4047
drodrigor <danierrodrigo@gmail.com>
2025-12-11 12:16:24 +0100

    Proyecto 8 Pregunta 2

---
+++ CODIGO/Día 8/Proyecto del día 8/misScripts.js
@@ -0,0 +1,13 @@
+// Array para almacenar empleados
+const empleados = [];
+
+// Constructor de Empleado
+class Empleado {
+constructor(nombre, apellido, edad, departamento, salario) {
+this.nombre = nombre;
+this.apellido = apellido;
+this.edad = edad;
+this.departamento = departamento;
+this.salario = salario;
+}
+}
