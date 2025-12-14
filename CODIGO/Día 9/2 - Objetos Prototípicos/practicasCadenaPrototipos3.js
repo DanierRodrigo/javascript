a4dc80bb1df43e06290ae348d392eb4425b50916
DanierRodrigo <danierrodrigo@gmail.com>
2025-12-14 23:59:18 +0100

    Ejercicio Cadena Prototipos 3 Enunciado

---
+++ CODIGO/Día 9/2 - Objetos Prototípicos/practicasCadenaPrototipos3.js
@@ -0,0 +1,12 @@
+/**
+Crea un constructor Futbolista, con las propiedades nombre, 
+equipo y posicion. Y luego, dentro de la función prototipos(), 
+cree una instancia futbolista1 mediante el constructor antes definido 
+con los siguientes datos: "Lionel Messi", "PSG", "Delantero". 
+Se pide que, en la misma función, se imprima en consola el resultado de 
+ejecutar el método valueOf() de la instancia futbolista1.
+ */
+
+function prototipos() {
+    
+}
