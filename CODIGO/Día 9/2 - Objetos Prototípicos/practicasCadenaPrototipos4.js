5e03e4bf97eab0a229b05da1c1216a9de6e78aa0
drodrigor <danierrodrigo@gmail.com>
2025-12-16 19:31:17 +0100

    Ejercicio Cadena Prototipos 4 Enunciado

---
+++ CODIGO/Día 9/2 - Objetos Prototípicos/practicasCadenaPrototipos4.js
@@ -0,0 +1,13 @@
+/**
+Crea un constructor Pelicula, con las propiedades titulo, 
+director y duracion. Y luego, dentro de la función prototipos(), 
+crea una instancia pelicula1 mediante el constructor antes definido 
+con los siguientes datos: "Jurassic Park", "Steven Spielberg", 127. 
+
+Se pide que, en la misma función, se imprima en consola 
+el resultado de ejecutar el método valueOf() de la instancia pelicula1 .
+ */
+
+function prototipos() {
+    
+}
