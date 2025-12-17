926ada3b2789db4c81916e6c6e08ca7b2b184c2c
drodrigor <danierrodrigo@gmail.com>
2025-12-17 14:28:14 +0100

    Enunciado ConstructorPrototipos1

---
+++ CODIGO/Día 9/2 - Objetos Prototípicos/practicasConstructorPrototipos1.js
@@ -0,0 +1,12 @@
+/**
+Crea un constructor Empleado, con las propiedades nombre, 
+antiguedad y sueldo. Luego se solicita crear la instancia empleado1 
+utilizando el constructor Empleado antes declarado. 
+
+Y por último, crea otra instancia empleado2, pero ahora utilizando 
+como objeto prototipo empleado1 para poner en práctica 
+el funcionamiento de la cadena de prototipos.
+ */
+
+let empleado1;
+let empleado2;
