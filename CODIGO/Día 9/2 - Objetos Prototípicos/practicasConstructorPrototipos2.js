ec37b8a6574074f59ddd3ca9fd2c89290795c18f
drodrigor <danierrodrigo@gmail.com>
2025-12-18 11:28:09 +0100

    Enunciado ConstructorPrototipos2

---
+++ CODIGO/Día 9/2 - Objetos Prototípicos/practicasConstructorPrototipos2.js
@@ -0,0 +1,12 @@
+/**
+Crea un constructor Restaurante, con las propiedades nombre, 
+direccion y ciudad. Luego se solicita crear la instancia restaurante1 
+utilizando el constructor Restaurante antes declarado. 
+
+Y por último, crea otra instancia restaurante2, pero ahora 
+utilizando como objeto prototipo restaurante1 para poner en práctica 
+el funcionamiento de la cadena de prototipos.
+ */
+
+let restaurante1;
+let restaurante2;
