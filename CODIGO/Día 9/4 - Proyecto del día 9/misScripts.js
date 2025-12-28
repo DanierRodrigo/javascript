4f504ab16f7473ff602b71bcaa566a0c3063d9a9
DanierRodrigo <danierrodrigo@gmail.com>
2025-12-28 23:53:55 +0100

    Pregunta 3 Proyecto 9

--- CODIGO/Día 9/4 - Proyecto del día 9/misScripts.js
+++ CODIGO/Día 9/4 - Proyecto del día 9/misScripts.js
@@ -5,3 +5,12 @@ function Automovil(marca, modelo, color, anio, titular) {
   this.anio = anio;
   this.titular = titular;
 }
+
+
+// Crear instancias
+let auto1 = new Automovil("Toyota", "Corolla", "Rojo", 2018, "Juan Pérez");
+let auto2 = new Automovil("Ford", "Fiesta", "Azul", 2020, "María Gómez");
+let auto3 = new Automovil("Honda", "Civic", "Negro", 2022, "Carlos López");
+
+// Array que contiene los automóviles
+let automoviles = [auto1, auto2, auto3];
