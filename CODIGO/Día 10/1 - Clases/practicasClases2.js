b6f5ab85e60a64cc7715c2bea84f411bf133ae04
DanierRodrigo <danierrodrigo@gmail.com>
2026-01-02 00:31:45 +0100

    Resuelto practicasClases2

--- CODIGO/Día 10/1 - Clases/practicasClases2.js
+++ CODIGO/Día 10/1 - Clases/practicasClases2.js
@@ -2,3 +2,10 @@
 Crear una clase TV que define mediante su constructor las propiedades: marca y pulgadas. 
 Utilizar la Expresión de Clase Anónima para definirlo.
  */
+
+const TV = class {
+  constructor(marca, pulgadas) {
+    this.marca = marca;
+    this.pulgadas = pulgadas;
+  }
+};
