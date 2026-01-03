670cbe14a6f32b03cdb6af661e42f93e812e013c
DanierRodrigo <danierrodrigo@gmail.com>
2026-01-03 20:58:23 +0100

    Enunciado practicasSubclases1

---
+++ CODIGO/Día 10/2 - Subclases/practicasSubclases1.js
@@ -0,0 +1,11 @@
+/**
+Dada la clase Persona ya definida, 
+crear la subclase Empleado (que hereda de Persona) pero que agrega la propiedad: legajo.
+ */
+
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
