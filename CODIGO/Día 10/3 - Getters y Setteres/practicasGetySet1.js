0ee89a39f845d22f53ab2d8d8ba138e9be067e1a
drodrigor <danierrodrigo@gmail.com>
2026-01-05 11:16:44 +0100

    Enunciado practicaGetYSet1

---
+++ CODIGO/Día 10/3 - Getters y Setteres/practicasGetySet1.js
@@ -0,0 +1,11 @@
+/**
+Dada la clase Ciudad, agregue los métodos get y set para las propiedades nombre, pais y cantHabitantes.
+ */
+
+class Ciudad {
+    constructor(nombre, pais, cantHabitantes) {
+        this._nombre = nombre;
+        this._pais = pais;
+        this._cantHabitantes = cantHabitantes;
+    }
+}
