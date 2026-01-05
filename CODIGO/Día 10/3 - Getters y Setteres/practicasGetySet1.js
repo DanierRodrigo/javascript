f35ce3686023d85154e2f6f7f859d236a39e15fb
drodrigor <danierrodrigo@gmail.com>
2026-01-05 11:19:48 +0100

    Resuelto practicasGetYSet1

--- CODIGO/Día 10/3 - Getters y Setteres/practicasGetySet1.js
+++ CODIGO/Día 10/3 - Getters y Setteres/practicasGetySet1.js
@@ -8,4 +8,31 @@ class Ciudad {
         this._pais = pais;
         this._cantHabitantes = cantHabitantes;
     }
+
+    // Getter y Setter para nombre
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(nuevoNombre) {
+        this._nombre = nuevoNombre;
+    }
+
+    // Getter y Setter para pais
+    get pais() {
+        return this._pais;
+    }
+
+    set pais(nuevoPais) {
+        this._pais = nuevoPais;
+    }
+
+    // Getter y Setter para cantHabitantes
+    get cantHabitantes() {
+        return this._cantHabitantes;
+    }
+
+    set cantHabitantes(nuevaCantidad) {
+        this._cantHabitantes = nuevaCantidad;
+    }
 }
