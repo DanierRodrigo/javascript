d41fe0656611b6e4c773bfac8b8ca049a8952ab2
drodrigor <danierrodrigo@gmail.com>
2026-01-05 11:58:36 +0100

    Resuelto practicasGetySets2

--- CODIGO/Día 10/3 - Getters y Setteres/practicasGetySet2.js
+++ CODIGO/Día 10/3 - Getters y Setteres/practicasGetySet2.js
@@ -8,4 +8,31 @@ class Colegio {
         this._direccion = direccion;
         this._cantAlumnos = cantAlumnos;
     }
+
+    // Getter y Setter para nombre
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(nuevoNombre) {
+        this._nombre = nuevoNombre;
+    }
+
+    // Getter y Setter para direccion
+    get direccion() {
+        return this._direccion;
+    }
+
+    set direccion(nuevaDireccion) {
+        this._direccion = nuevaDireccion;
+    }
+
+    // Getter y Setter para cantAlumnos
+    get cantAlumnos() {
+        return this._cantAlumnos;
+    }
+
+    set cantAlumnos(nuevaCantidad) {
+        this._cantAlumnos = nuevaCantidad;
+    }
 }
