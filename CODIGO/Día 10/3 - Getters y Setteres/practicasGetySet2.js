ea32451e1723c18a60c7435417b2ac84da43c4e7
drodrigor <danierrodrigo@gmail.com>
2026-01-05 11:57:40 +0100

    Enunciado practicasGetySets2

---
+++ CODIGO/Día 10/3 - Getters y Setteres/practicasGetySet2.js
@@ -0,0 +1,11 @@
+/**
+Dada la clase Colegio, agregue los métodos get y set para las propiedades nombre, direccion y cantAlumnos.
+ */
+
+class Colegio {
+    constructor(nombre, direccion, cantAlumnos) {
+        this._nombre = nombre;
+        this._direccion = direccion;
+        this._cantAlumnos = cantAlumnos;
+    }
+}
