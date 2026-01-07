edd22215b41b1e1fb1c10dd28ea926cdd32b6a0a
drodrigor <danierrodrigo@gmail.com>
2026-01-07 12:14:36 +0100

    Pregunta 3 Proyecto 10

--- CODIGO/Día 10/4 - Proyecto del día 10/misScripts.js
+++ CODIGO/Día 10/4 - Proyecto del día 10/misScripts.js
@@ -9,3 +9,36 @@ class Animal {
         return `${this.nombre} - ${this.peso} kg - ${this.edad} años`;
     }
 }
+
+class Perro extends Animal {
+    constructor(nombre, peso, edad, raza) {
+        super(nombre, peso, edad);
+        this.raza = raza;
+    }
+
+    informacion() {
+        return `Perro - ${this.nombre} - ${this.peso} kg - ${this.edad} años - ${this.raza}`;
+    }
+}
+
+class Gato extends Animal {
+    constructor(nombre, peso, edad, sexo) {
+        super(nombre, peso, edad);
+        this.sexo = sexo;
+    }
+
+    informacion() {
+        return `Gato - ${this.nombre} - ${this.peso} kg - ${this.edad} años - ${this.sexo}`;
+    }
+}
+
+class Conejo extends Animal {
+    constructor(nombre, peso, edad, color) {
+        super(nombre, peso, edad);
+        this.color = color;
+    }
+
+    informacion() {
+        return `Conejo - ${this.nombre} - ${this.peso} kg - ${this.edad} años - Color: ${this.color}`;
+    }
+}
