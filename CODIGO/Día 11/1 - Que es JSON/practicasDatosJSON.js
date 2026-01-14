bb82388ddaeca60a57cf2f0789ed105f263e4a9f
DanierRodrigo <danierrodrigo@gmail.com>
2026-01-14 23:48:28 +0100

    Enunciado practicasDatosJSON1

---
+++ CODIGO/Día 11/1 - Que es JSON/practicasDatosJSON.js
@@ -0,0 +1,16 @@
+/**
+Dado el archivo auxiliar datos.json, 
+obtener los datos de dicho archivo utilizando XMLHttpRequest 
+y asigne cada campo a la variable correspondiente ya declarada.
+ */
+
+let titulo;
+let director;
+let lanzamiento;
+let duracion;
+let actores;
+let premioOscar;
+
+function obtenerDatos() {
+    
+}
