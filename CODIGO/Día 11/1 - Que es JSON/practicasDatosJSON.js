bb995694d5e668f74e279f7d6a19959387a31d6b
drodrigor <danierrodrigo@gmail.com>
2026-01-15 19:12:26 +0100

    Resuelto practicasDatosJSON1

--- CODIGO/Día 11/1 - Que es JSON/practicasDatosJSON.js
+++ CODIGO/Día 11/1 - Que es JSON/practicasDatosJSON.js
@@ -12,5 +12,24 @@ let actores;
 let premioOscar;
 
 function obtenerDatos() {
-    
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "datos.json", true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const datos = JSON.parse(xhr.responseText);
+
+            titulo = datos.titulo;
+            director = datos.director;
+            lanzamiento = datos.lanzamiento;
+            duracion = datos.duracion;
+            actores = datos.actores;
+            premioOscar = datos.premioOscar;
+
+            // Opcional: comprobar resultados en consola
+            console.log(titulo, director, lanzamiento, duracion, actores, premioOscar);
+        }
+    };
+
+    xhr.send();
 }
