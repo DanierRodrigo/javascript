6b3095a1b250f5ae63d82e0d2a61c1c485957b18
drodrigor <danierrodrigo@gmail.com>
2026-01-20 17:29:49 +0100

    Enunciado practicasDatosJSON2

---
+++ CODIGO/Día 11/1 - Que es JSON/practicasDatosJSON2.js
@@ -0,0 +1,15 @@
+/**
+Dado el archivo auxiliar datos.json, 
+obtener los datos de dicho archivo utilizando XMLHttpRequest 
+y asigne cada campo a la variable correspondiente ya declarada.
+ */
+
+let nombre;
+let calle;
+let ciudad;
+let cantEstudiantes;
+let estudiantes;
+
+function obtenerDatos() {
+    
+}
