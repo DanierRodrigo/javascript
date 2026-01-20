42dd09e6d07a4ca0c6141ab443b9b4d7e5fe163e
drodrigor <danierrodrigo@gmail.com>
2026-01-20 17:37:02 +0100

    Resuelto practicasDatosJSON2

--- CODIGO/Día 11/1 - Que es JSON/practicasDatosJSON2.js
+++ CODIGO/Día 11/1 - Que es JSON/practicasDatosJSON2.js
@@ -11,5 +11,23 @@ let cantEstudiantes;
 let estudiantes;
 
 function obtenerDatos() {
-    
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "datos.json", true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const datos = JSON.parse(xhr.responseText);
+
+            nombre = datos.nombre;
+            calle = datos.direccion.calle;
+            ciudad = datos.direccion.ciudad;
+            cantEstudiantes = datos.cantEstudiantes;
+            estudiantes = datos.estudiantes;
+
+            // Opcional: verificar en consola
+            console.log(nombre, calle, ciudad, cantEstudiantes, estudiantes);
+        }
+    };
+
+    xhr.send();
 }
