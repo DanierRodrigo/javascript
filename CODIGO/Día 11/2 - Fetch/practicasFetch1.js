4e9715f9765c777170e6393b013d54dde332deb0
drodrigor <danierrodrigo@gmail.com>
2026-01-23 14:41:15 +0100

    Resuelto practicasFetch1

--- CODIGO/Día 11/2 - Fetch/practicasFetch1.js
+++ CODIGO/Día 11/2 - Fetch/practicasFetch1.js
@@ -12,5 +12,20 @@ let actores;
 let premioOscar;
 
 function obtenerDatos() {
-    
+    fetch("datos.json")
+        .then(response => response.json())
+        .then(data => {
+            titulo = data.titulo;
+            director = data.director;
+            lanzamiento = data.lanzamiento;
+            duracion = data.duracion;
+            actores = data.actores;
+            premioOscar = data.premioOscar;
+
+            // Opcional: comprobar en consola
+            console.log(titulo, director, lanzamiento, duracion, actores, premioOscar);
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+        });
 }
