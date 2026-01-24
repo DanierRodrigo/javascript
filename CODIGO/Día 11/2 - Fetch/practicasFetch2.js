c576d733dbee774a2f7b999b90795a2932f057ed
DanierRodrigo <danierrodrigo@gmail.com>
2026-01-24 23:59:30 +0100

    Enunciado practicasFetch2

---
+++ CODIGO/Día 11/2 - Fetch/practicasFetch2.js
@@ -0,0 +1,15 @@
+/**
+Dado el archivo auxiliar datos1.json, 
+obtener los datos de dicho archivo utilizando fetch
+y asigne cada campo a la variable correspondiente ya declarada.
+ */
+
+let nombre;
+let calle;
+let ciudad;
+let cantEstudiantes;
+let estudiantes;
+
+function obtenerDatos() {
+    
+}
