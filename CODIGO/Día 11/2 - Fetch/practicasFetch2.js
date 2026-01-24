ad34f8ab67f50229ed18ccbfd60afef91e33b61c
DanierRodrigo <danierrodrigo@gmail.com>
2026-01-25 00:01:33 +0100

    Resuelto practicasFetch2

--- CODIGO/Día 11/2 - Fetch/practicasFetch2.js
+++ CODIGO/Día 11/2 - Fetch/practicasFetch2.js
@@ -11,5 +11,22 @@ let cantEstudiantes;
 let estudiantes;
 
 function obtenerDatos() {
-    
+    fetch("datos1.json")
+        .then(response => response.json())
+        .then(data => {
+            nombre = data.nombre;
+            calle = data.direccion.calle;
+            ciudad = data.direccion.ciudad;
+            cantEstudiantes = data.cantEstudiantes;
+            estudiantes = data.estudiantes;
+
+            console.log(nombre);
+            console.log(calle);
+            console.log(ciudad);
+            console.log(cantEstudiantes);
+            console.log(estudiantes);
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+        });
 }
