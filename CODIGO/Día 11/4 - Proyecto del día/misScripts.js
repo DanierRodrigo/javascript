a80a23cdebfad9211c9392b965c2bc126f8d6546
drodrigor <danierrodrigo@gmail.com>
2026-02-04 12:56:51 +0100

    Pregunta 4 Proyecto 11

--- CODIGO/Día 11/4 - Proyecto del día/misScripts.js
+++ CODIGO/Día 11/4 - Proyecto del día/misScripts.js
@@ -1,17 +1,16 @@
-fetch("resumen.json")
-    .then(response => response.json())
-    .then(data => {
+function obtenerResumen() {
+    fetch("resumen.json")
+        .then(response => response.json())
+        .then(data => {
 
-        // Accedemos a los campos con el punto (.)
-        document.getElementById("banco").textContent = data.banco;
-        document.getElementById("sucursal").textContent = data.sucursal;
-        document.getElementById("titular").textContent = data.titular;
-        document.getElementById("nro_cuenta").textContent = data.nro_cuenta;
-        document.getElementById("cbu").textContent = data.cbu;
-        document.getElementById("abierto").textContent = data.abierto;
+            document.getElementById("banco").textContent = data.banco;
+            document.getElementById("sucursal").textContent = data.sucursal;
+            document.getElementById("titular").textContent = data.titular;
+            document.getElementById("nro_cuenta").textContent = data.nro_cuenta;
+            document.getElementById("cbu").textContent = data.cbu;
+            document.getElementById("abierto").textContent = data.abierto;
 
-        // Accedemos al array saldo y luego a sus campos
-        document.getElementById("saldo_usd").textContent = data.saldo[0].monto;
-        document.getElementById("saldo_eur").textContent = data.saldo[1].monto;
-
-    });
+            document.getElementById("saldo_usd").textContent = data.saldo[0].monto;
+            document.getElementById("saldo_eur").textContent = data.saldo[1].monto;
+        });
+}
