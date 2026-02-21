950af9a114b41914cf55c99a2f65b1800e2b4109
DanierRodrigo <danierrodrigo@gmail.com>
2026-02-21 23:56:56 +0100

    Enunciado practicaEventosTeclado2

---
+++ CODIGO/Día 12/3 - Eventos de Teclado/practicaEventosTeclado2.js
@@ -0,0 +1,12 @@
+/**
+Dado el código HTML proporcionado, 
+programar la función agregarEvento() del archivo index.js, 
+que agregue un evento keydown al input miInput. 
+Al disparar el evento, deberá validar que el usuario no pueda ingresar 
+en el input la barra espaciadora si la presiona. 
+El número keyCode de la barra espaciadora es 32.
+ */
+
+function agregarEvento() {
+    
+}
