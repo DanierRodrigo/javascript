db9b4f78a041965a9828e93c99a550db8751f49a
drodrigor <danierrodrigo@gmail.com>
2026-02-25 15:36:13 +0100

    Enunciado practicasEventosRaton1

---
+++ CODIGO/Día 12/4 - Eventos de Ratón/practicasEventosRaton1.js
@@ -0,0 +1,9 @@
+/**
+Dado el código HTML proporcionado, programar la función agregarEvento() del archivo index.js, 
+que agregue un evento mouseover al boton miBoton. 
+Al disparar el evento, deberá imprimir en consola el mensaje: Mouse sobre botón
+ */
+
+function agregarEvento() {
+    
+}
