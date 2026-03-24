01ef086690e1e4c1b8b25fba8f67c394056d7e7a
drodrigor <danierrodrigo@gmail.com>
2026-03-24 20:09:30 +0100

    Enunciado practicasEventosRaton2

---
+++ CODIGO/Día 12/4 - Eventos de Ratón/practicasEventosRaton2.js
@@ -0,0 +1,9 @@
+/**
+Dado el código HTML proporcionado, programar la función agregarEvento() del archivo index.js, 
+que agregue un evento mouseout al boton miBoton. 
+Al disparar el evento, deberá imprimir en consola el mensaje: Mouse por fuera de botón
+ */
+
+function agregarEvento() {
+    
+}
