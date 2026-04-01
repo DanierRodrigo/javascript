d3003a758b29989481149d92137877f2389f9877
drodrigor <danierrodrigo@gmail.com>
2026-04-01 23:47:05 +0200

    Enunciado practicasEventosTiempoReal

---
+++ CODIGO/Día 12/5 - Eventos en Tiempo Real/practicasEventosTiempoReal1.js
@@ -0,0 +1,7 @@
+/**
+Declarar una variable socket, ç
+que establezca una comunicación con el servidor ws://localhost:8080 
+y a través del mismo, enviar el mensaje "¡Hola mundo!"
+ */
+
+let socket;
