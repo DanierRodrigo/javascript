fe763ed82443680165ac59d44eac5af640f39d1f
DanierRodrigo <danierrodrigo@gmail.com>
2026-04-02 22:57:30 +0200

    Enunciado practicasEventosTiempoReal2

---
+++ CODIGO/Día 12/5 - Eventos en Tiempo Real/practicasEventosTiempoReal2.js
@@ -0,0 +1,7 @@
+/**
+Declarar una variable socket, que establezca una comunicación con el servidor ws://localhost:4200 
+y que capture el evento onmessage para recibir mensajes del servidor. 
+Al dispararse el evento, se pide imprimir en consola el contenido del mensaje mediante event.data
+ */
+
+let socket;
